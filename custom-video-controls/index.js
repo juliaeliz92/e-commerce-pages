fc0c19072351063a7338e33abbff3c85a7c6cfc6
Julia Paul <juliapaulmyla@yahoo.com>
2024-11-07 01:02:11 -0500

    update time

--- custom-video-controls/index.js
+++ custom-video-controls/index.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll('button')
 const media = document.querySelector('#media')
 const rail = document.querySelector('#video-seek-rail')
 const fill = document.querySelector('#video-seek-fill')
+const timeLeft = document.querySelector('#time-left')
+const duration = document.querySelector('#time-duration')
 
 for(let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', (e) => {
@@ -13,8 +15,30 @@ for(let i = 0; i < buttons.length; i++) {
     })
 }
 
-rail.addEventListener('click', seekTime)
-
 const seekTime = (e) => {
     media.currentTime = e.offsetX / rail.offsetWidth * media.duration;
 }
+
+const appendZero = (time) => {
+    return time >= 10 ? time : `0${time}`
+}
+
+const displayTime = () => {
+    const secLeft = (media.currentTime).toFixed(0)
+    const minLeft = (media.currentTime / 60).toFixed(0)
+    const secDuration = (media.duration).toFixed(0)
+    const minDuration = (media.duration / 60).toFixed(0)
+    timeLeft.innerHTML = `${appendZero(minLeft)} : ${appendZero(secLeft)}`
+    duration.innerHTML = `${appendZero(minDuration)} : ${appendZero(secDuration)}`
+}
+
+const updateTime = () => {
+    let currentLength = rail.clientWidth * (media.currentTime / media.duration)
+    fill.style.width = `${currentLength}px`
+    displayTime(media.currentTime)
+}
+
+rail.addEventListener('click', seekTime)
+fill.addEventListener('click', seekTime)
+media.addEventListener('loadedmetadata', updateTime)
+media.addEventListener('timeupdate', updateTime)
